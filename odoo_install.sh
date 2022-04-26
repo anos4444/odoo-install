32f4a40f8daab02474dafcbdea7f7b3fb122a396
anos4444 <anas.abdullah@gmail.com>
2022-04-26 05:23:42 +0300

    Modified NGINX Script

--- odoo_install.sh
+++ odoo_install.sh
@@ -21,14 +21,14 @@ OE_HOME_EXT="/$OE_USER/${OE_USER}-server"
 # Set to true if you want to install it, false if you don't need it or have it already installed.
 INSTALL_WKHTMLTOPDF="True"
 # Set the default Odoo port (you still have to use -c /etc/odoo-server.conf for example to use this.)
-OE_PORT="8069"
+OE_PORT="8015"
 # Choose the Odoo version which you want to install. For example: 13.0, 12.0, 11.0 or saas-18. When using 'master' the master version will be installed.
 # IMPORTANT! This script contains extra libraries that are specifically needed for Odoo 13.0
 OE_VERSION="15.0"
 # Set this to True if you want to install the Odoo enterprise version!
 IS_ENTERPRISE="False"
 # Set this to True if you want to install Nginx!
-INSTALL_NGINX="False"
+INSTALL_NGINX="True"
 # Set the superadmin password - if GENERATE_RANDOM_PASSWORD is set to "True" we will automatically generate a random password, otherwise we use this one
 OE_SUPERADMIN="admin"
 # Set to "True" to generate a random password, "False" to use the variable in OE_SUPERADMIN
@@ -268,78 +268,76 @@ if [ $INSTALL_NGINX = "True" ]; then
   echo -e "\n---- Installing and setting up Nginx ----"
   sudo apt install nginx -y
   cat <<EOF > ~/odoo
-server {
-  listen 80;
 
-  # set proper server name after domain set
-  server_name $WEBSITE_NAME;
-
-  # Add Headers for odoo proxy mode
-  proxy_set_header X-Forwarded-Host \$host;
-  proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-  proxy_set_header X-Forwarded-Proto \$scheme;
-  proxy_set_header X-Real-IP \$remote_addr;
-  add_header X-Frame-Options "SAMEORIGIN";
-  add_header X-XSS-Protection "1; mode=block";
-  proxy_set_header X-Client-IP \$remote_addr;
-  proxy_set_header HTTP_X_FORWARDED_HOST \$remote_addr;
-
-  #   odoo    log files
-  access_log  /var/log/nginx/$OE_USER-access.log;
-  error_log       /var/log/nginx/$OE_USER-error.log;
-
-  #   increase    proxy   buffer  size
-  proxy_buffers   16  64k;
-  proxy_buffer_size   128k;
-
-  proxy_read_timeout 900s;
-  proxy_connect_timeout 900s;
-  proxy_send_timeout 900s;
-
-  #   force   timeouts    if  the backend dies
-  proxy_next_upstream error   timeout invalid_header  http_500    http_502
-  http_503;
-
-  types {
-    text/less less;
-    text/scss scss;
-  }
-
-  #   enable  data    compression
-  gzip    on;
-  gzip_min_length 1100;
-  gzip_buffers    4   32k;
-  gzip_types  text/css text/less text/plain text/xml application/xml application/json application/javascript application/pdf image/jpeg image/png;
-  gzip_vary   on;
-  client_header_buffer_size 4k;
-  large_client_header_buffers 4 64k;
-  client_max_body_size 0;
-
-  location / {
-    proxy_pass    http://127.0.0.1:$OE_PORT;
-    # by default, do not forward anything
-    proxy_redirect off;
-  }
-
-  location /longpolling {
-    proxy_pass http://127.0.0.1:$LONGPOLLING_PORT;
-  }
-
-  location ~* .(js|css|png|jpg|jpeg|gif|ico)$ {
-    expires 2d;
-    proxy_pass http://127.0.0.1:$OE_PORT;
-    add_header Cache-Control "public, no-transform";
-  }
-
-  # cache some static data in memory for 60mins.
-  location ~ /[a-zA-Z0-9_-]*/static/ {
-    proxy_cache_valid 200 302 60m;
-    proxy_cache_valid 404      1m;
-    proxy_buffering    on;
-    expires 864000;
-    proxy_pass    http://127.0.0.1:$OE_PORT;
-  }
+upstream jasani{
+	server 127.0.0.1:8015 weight=1 fail_timeout=0;
 }
+
+upstream jasani-im {
+	server 127.0.0.1:8072 weight=1 fail_timeout=0;
+}
+
+server {
+	server_name	$WEBSITE_NAME;
+
+	client_max_body_size 1000m;
+	keepalive_timeout 6000;
+	proxy_buffers 16 64k;
+	proxy_buffer_size 128k;
+	proxy_connect_timeout 60000s;
+	proxy_send_timeout 60000s;
+	proxy_read_timeout 60000s;
+	proxy_next_upstream error timeout invalid_header http_500 http_502 http_503;
+	proxy_set_header Host $host;
+	proxy_set_header X-Forwarded-Host $host;
+	proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+	proxy_set_header X-Forwarded-Proto $scheme;
+	proxy_set_header X-Real-IP $remote_addr;
+
+	proxy_redirect off;
+	proxy_buffering off;
+
+	location / {
+		proxy_pass http://jasani;
+	}
+
+	location /longpolling {
+		proxy_pass http://jasani-im;
+	}
+
+	location /web/database/manager{
+		auth_basic "Restricted";
+		auth_basic_user_file /etc/nginx/.htpasswd;
+		proxy_pass http://jasani;
+	}
+
+	location /web/static/ {
+	proxy_cache_valid 200 60m;
+	proxy_buffering on;
+	expires 864000;
+	proxy_pass http://jasani;
+	}
+
+ gzip_types text/css text/less text/plain text/xml application/xml application/json application/javascript;
+ gzip on;
+
+	listen 443 ssl; # managed by Certbot
+
+    ssl_certificate /etc/letsencrypt/live/$WEBSITE_NAME/fullchain.pem; # managed by Certbot
+    ssl_certificate_key /etc/letsencrypt/live/$WEBSITE_NAME/privkey.pem; # managed by Certbot
+}
+
+
+server {
+    if ($host = $WEBSITE_NAME) {
+        return 301 https://$host$request_uri;
+    } # managed by Certbot
+
+ server_name $WEBSITE_NAME;
+    listen 80;
+    return 404; # managed by Certbot
+}
+
 EOF
 
   sudo mv ~/odoo /etc/nginx/sites-available/$WEBSITE_NAME
